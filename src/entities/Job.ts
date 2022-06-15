87637a713753c15781eb9bf660a40f31d295a133
Leonardo <leonardo.neves.external@k2partnering.com>
2022-06-15 11:11:05 -0300

    chore: add entities

---
+++ src/entities/Job.ts
@@ -0,0 +1,4 @@
+export class Job {
+    public readonly id: number;
+    public description: string;
+}
---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { response, Router } from "express";
+
+const router = Router()
+
+
+router.post('/users', (req, res) => {
+    return response.status(201).send();
+})
+
+export { router }
